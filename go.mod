20328392a2d035b14d8b9fb2837ad58714661752
gaohongyu.hygao <gaohongyu.hygao@bytedance.com>
2023-05-14 00:30:36 +0800

    feat: 完成 v2.0.2 版本的代码

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/wqvoon/testlib1
+module github.com/wqvoon/testlib1/v2
 
 go 1.18
--- run.go
+++ run.go
@@ -4,12 +4,14 @@ import "fmt"
 
 var globalMap map[string]string = make(map[string]string)
 
+const prefix = "testlib1@v2.0.2 "
+
 func Register(k, v string) {
-	fmt.Println("testlib1@v2.0.1 Register")
+	fmt.Println(prefix + "Register")
 	globalMap[k] = v
 }
 
 func GetAll() {
-	fmt.Println("testlib1@v2.0.1 GetAll")
+	fmt.Println(prefix + "GetAll")
 	fmt.Println(globalMap)
 }

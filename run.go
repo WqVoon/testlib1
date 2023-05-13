4d11bf75b20939f4bb875ffc6d2414b9e2770491
gaohongyu.hygao <gaohongyu.hygao@bytedance.com>
2023-05-13 22:54:26 +0800

    feat: 完成 v0.0.2 版本代码

--- run.go
+++ run.go
@@ -5,11 +5,11 @@ import "fmt"
 var globalMap map[string]string = make(map[string]string)
 
 func Register(k, v string) {
-	fmt.Println("testlib1@v0.0.1 Register")
+	fmt.Println("testlib1@v0.0.2 Register")
 	globalMap[k] = v
 }
 
 func GetAll() {
-	fmt.Println("testlib1@v0.0.1 GetAll")
+	fmt.Println("testlib1@v0.0.2 GetAll")
 	fmt.Println(globalMap)
 }
